6b05c1ea973e8a3edb1b15557335db7145ce63a5
Farzad <farzadrahman59@gmail.com>
2019-05-11 11:30:10 +0600

    Farzad

--- app/Http/Controllers/PrescriptionController.php
+++ app/Http/Controllers/PrescriptionController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Appointment;
 use Illuminate\Http\Request;
+use Mike42\Escpos\PrintConnectors\FilePrintConnector;
+use Mike42\Escpos\Printer;
 
 class PrescriptionController extends Controller
 {
@@ -17,6 +19,13 @@ class PrescriptionController extends Controller
    }
 
    public function getPrescription($id){
+//       $connector = new FilePrintConnector("php://stdout");
+//       $printer = new Printer($connector);
+//       $printer -> text("Hello World!\n");
+//       $printer -> cut();
+//       $printer -> close();
+//
+//       return "printed";
         $appointment=Appointment::select('appointment.*','patient.*','doctor.*','appointment.id as regNo')
             ->leftJoin('patient','patient.id','appointment.fkpatientId')
             ->leftJoin('doctor','doctor.id','appointment.fkdoctorId')
